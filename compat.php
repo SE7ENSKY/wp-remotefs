57ff4d9e8f81d7fa35d2bf380f9ed41b25a33608
Ivan Kravchenko <i@se7ensky.com>
2014-10-29 21:12:04 +0200

    better root-relative-urls plugin compatibility

--- compat.php
+++ compat.php
@@ -15,10 +15,15 @@ function rfs_compat_root_relative_urls() {
 		'MP_WP_Root_Relative_URLS',
 		'root_relative_image_urls'
 	);
+	$rru_root_relative_media_urls = array(
+		'MP_WP_Root_Relative_URLS',
+		'root_relative_media_urls'
+	);
 	
 	remove_filter('upload_dir', $rru_fix_upload_paths, 1);
 	remove_filter('attachment_link', $rru_proper_root_relative_url, 1);
 	remove_filter('wp_get_attachment_url', $rru_proper_root_relative_url, 1);
 	remove_filter('image_send_to_editor', $rru_root_relative_image_urls, 1);
 	remove_filter('media_send_to_editor', $rru_root_relative_image_urls, 1);
+	remove_filter('media_send_to_editor', $rru_root_relative_media_urls, 1);
 }
